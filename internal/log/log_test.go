56b6e4452bdb29291e290547dd94190f08e826b3
mind1949 <lianjie1949@gmail.com>
2022-02-28 19:39:23 +0800

    22年2月28号

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -1,106 +1,106 @@
 package log
 
 import (
+	"io"
 	"os"
 	"testing"
 
+	api "github.com/mind1949/proglog/api/v1"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
-var (
-	write = []byte("hello world")
-	width = uint64(len(write) + lenWidth)
-)
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T, log *Log,
+	){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"init with existing segments":       testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := os.MkdirTemp("", "store-test")
+			require.NoError(t, err)
+			defer os.RemoveAll(dir)
 
-func TestStoreAppendRead(t *testing.T) {
-	f, err := os.CreateTemp("", "store_append_read_test")
-	require.NoError(t, err)
-	defer os.Remove(f.Name())
-
-	s, err := newStore(f)
-	require.NoError(t, err)
-
-	testAppend(t, s)
-	testRead(t, s)
-	testReadAt(t, s)
-
-	s, err = newStore(f)
-	require.NoError(t, err)
-	testRead(t, s)
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+			fn(t, log)
+		})
+	}
 }
 
-func testAppend(t *testing.T, s *store) {
-	t.Helper()
-	for i := uint64(1); i < 4; i++ {
-		n, pos, err := s.Append(write)
+func testAppendRead(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	off, err := log.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, record.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.Nil(t, read)
+	require.Error(t, err)
+}
+
+func testInitExisting(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(record)
 		require.NoError(t, err)
-		require.Equal(t, pos+n, width*i)
 	}
+	require.NoError(t, log.Close())
+
+	off, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	n, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+
+	off, err = n.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+	off, err = n.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
 }
 
-func testRead(t *testing.T, s *store) {
-	t.Helper()
-	var pos uint64
-	for i := uint64(1); i < 4; i++ {
-		read, err := s.Read(pos)
+func testReader(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	off, err := log.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := io.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &api.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, record.Value, read.Value)
+}
+
+func testTruncate(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(record)
 		require.NoError(t, err)
-		require.Equal(t, write, read)
-		pos += width
 	}
-}
+	err := log.Truncate(1)
 
-func testReadAt(t *testing.T, s *store) {
-	t.Helper()
-	for i, off := uint64(1), int64(0); i < 4; i++ {
-		b := make([]byte, lenWidth)
-		n, err := s.ReadAt(b, off)
-		require.NoError(t, err)
-		require.Equal(t, n, lenWidth)
-		off += int64(n)
-
-		size := enc.Uint64(b)
-		b = make([]byte, size)
-		n, err = s.ReadAt(b, off)
-		require.NoError(t, err)
-		require.Equal(t, b, write)
-		require.Equal(t, n, len(b))
-		off += int64(n)
-	}
-}
-
-func TestStoreClose(t *testing.T) {
-	f, err := os.CreateTemp("", "store_close_test")
-	require.NoError(t, err)
-	defer os.Remove(f.Name())
-
-	s, err := newStore(f)
-	require.NoError(t, err)
-	_, _, err = s.Append(write)
-	require.NoError(t, err)
-
-	f, beforeSize, err := openFile(f.Name())
-	require.NoError(t, err)
-
-	err = s.Close()
-	require.NoError(t, err)
-
-	_, aftbeforeSize, err := openFile(f.Name())
-	require.NoError(t, err)
-	require.True(t, beforeSize < aftbeforeSize)
-}
-
-func openFile(name string) (file *os.File, size int64, err error) {
-	f, err := os.OpenFile(
-		name,
-		os.O_RDWR|os.O_CREATE|os.O_APPEND,
-		0644,
-	)
-	if err != nil {
-		return nil, 0, err
-	}
-	fi, err := f.Stat()
-	if err != nil {
-		return nil, 0, err
-	}
-	return f, fi.Size(), nil
+	_, err = log.Read(0)
+	require.Error(t, err)
 }
---
+++ internal/log/store_test.go
@@ -0,0 +1,106 @@
+package log
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write) + lenWidth)
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := os.CreateTemp("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, n, lenWidth)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, b, write)
+		require.Equal(t, n, len(b))
+		off += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := os.CreateTemp("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+
+	err = s.Close()
+	require.NoError(t, err)
+
+	_, aftbeforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, beforeSize < aftbeforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
